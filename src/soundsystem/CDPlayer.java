e5ab26e31b3fcacd791f5a11b8db0c106264b07e
Ryan Williams <ryan.williams@projectpeople.com>
2017-12-06 10:06:39 +0000

    Initial commit

---
+++ src/soundsystem/CDPlayer.java
@@ -0,0 +1,18 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CDPlayer implements MediaPlayer {
+
+    private CompactDisc cd;
+
+    public CDPlayer(CompactDisc cd){
+        this.cd = cd;
+    }
+
+    public void play(){
+        cd.play();
+    }
+
+}
